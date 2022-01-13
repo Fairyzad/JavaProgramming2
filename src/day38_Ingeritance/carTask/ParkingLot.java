8d38161e75922f8329105e1800f4890ebb03d5e8
peri <parizat@live.com>
2022-01-13 08:47:40 -0800

    update

---
+++ src/day38_Ingeritance/carTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Ingeritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camery",2020,20000,
+                "White",234879);
+
+        Tesla tesla = new Tesla("Model S", 2022, 94990, "Red", 0);
+
+        BMW bmw = new BMW("X5", 2020, 67350, "black", 12000);
+
+    }
+
+}
